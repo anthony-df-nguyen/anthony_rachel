9eb7c353d802e12c23b660a5c3d477fabc9af982
Anthony Nguyen <anthonynguyen@Anthony-Personal-Macbook.local>
2025-05-05 21:32:37 -0700

    pic time

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,11 +1,13 @@
 import Section from "@/components/Section";
-import {MyImageGallery} from "@/components/ImageGallery";
+//import {MyImageGallery} from "@/components/ImageGallery";
+import PicflowEmbed from "@/components/PicTimeGallery";
 
 export default function Gallery() {
   return (
     <div className="container mx-auto">
       <Section title="Our Gallery" id="our_gallery">
-        <MyImageGallery />
+        <PicflowEmbed />
+        {/* <MyImageGallery /> */}
       </Section>
     </div>
   );
